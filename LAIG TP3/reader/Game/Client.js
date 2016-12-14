4e210758f05d48e3cbb76e8ef9ee9c46321b1ee2
PedroPachecoInf <pedrodanielpacheco96@hotmail.com>
2016-12-14 11:13:09 +0000

    Client.js created

---
+++ LAIG TP3/reader/Game/Client.js
@@ -0,0 +1,23 @@
+/**
+* Client
+*
+*/
+
+function Client() {
+ console.log("Client Iniciated");
+};
+
+Client.prototype.constructor = Client;
+
+Client.prototype.getPrologRequest = function(requestString, onSuccess, onError, port)
+			{
+				var requestPort = port || 8081
+				var request = new XMLHttpRequest();
+				request.open('GET', 'http://localhost:'+requestPort+'/'+requestString, true);
+
+				request.onload = onSuccess || function(data){console.log("Request successful. Reply: " + data.target.response);};
+				request.onerror = onError || function(){console.log("Error waiting for response");};
+
+				request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+				request.send();
+			}
